0cb514654f6fbb0e2530f0fdc4d33e20cae6cc8a
Donggeun Lee <piou987@gmail.com>
2022-03-04 19:58:26 +0900

    Feat: Implement ec2 security group cleaner

---
+++ ec2/security_group.py
@@ -0,0 +1,98 @@
+from pprint import pprint
+import boto3
+
+def list_security_group_ids() -> list:
+    try:
+        # init boto3 client
+        client = boto3.client('ec2')
+        # describe all security groups
+        response = client.describe_security_groups()
+        # parse and return security group ids from above response
+        # it exclude default security group
+        return [ 
+            security_group['GroupId']
+            for security_group in response['SecurityGroups']
+            if security_group['GroupName'] != 'default' ]
+    except Exception as error:
+        print(error)
+        exit(1)
+
+def revoke_security_group_rules(security_group_ids: list) -> list:
+    try:
+        # init boto3 client
+        client = boto3.client('ec2')
+        # describe all security group rules
+        response = client.describe_security_group_rules(
+            Filters=[
+                {
+                    'Name': 'group-id',
+                    'Values': security_group_ids
+                }
+            ])
+        security_group_rules = response['SecurityGroupRules']
+        # revoke all security group rules
+        for security_group_id in security_group_ids:
+            egress_rule_ids = [
+                security_group_rule['SecurityGroupRuleId']
+                for security_group_rule in security_group_rules
+                if security_group_rule['GroupId'] == security_group_id and security_group_rule['IsEgress']
+            ]
+            ingress_rule_ids = [
+                security_group_rule['SecurityGroupRuleId']
+                for security_group_rule in security_group_rules
+                if security_group_rule['GroupId'] == security_group_id and not security_group_rule['IsEgress']
+            ]
+            if egress_rule_ids: client.revoke_security_group_egress(
+                GroupId=security_group_id,
+                SecurityGroupRuleIds=egress_rule_ids)
+            if ingress_rule_ids: client.revoke_security_group_ingress(
+                GroupId=security_group_id,
+                SecurityGroupRuleIds=ingress_rule_ids)
+        # return successfully code, if done.
+        return True
+    except Exception as error:
+        print(error)
+        exit(1)
+
+def delete_security_groups(security_group_ids: list) -> bool:
+    try:
+        # init boto3 client
+        client = boto3.client('ec2')
+        # delete security groups
+        for security_group_id in security_group_ids:
+            client.delete_security_group(GroupId=security_group_id)
+        # if successfully code, if done.
+        return True
+    except Exception as error:
+        print(error)
+        exit(1)
+
+def security_group_cleaner() -> bool:
+    try:
+        # list up all resources at rds service
+        security_group_ids = list_security_group_ids()
+
+        # output will deleted resources list
+        print("==== EC2 Security Groups ====")
+        pprint(security_group_ids)
+
+        # approval and delete all resources
+        while True:
+            confirm = input("Are you sure you want to delete (y/n)? ")
+            if confirm == "y":
+                revoke_security_group_rules(security_group_ids)
+                delete_security_groups(security_group_ids)
+                return True
+            elif confirm == "n":
+                print("Canceled")
+                exit(1)
+            else:
+                print("Only input 'y' or 'n', Try again! ")
+    except Exception as error:
+        print(error)
+        exit(1)
+
+if __name__ == '__main__':
+    result = security_group_cleaner()
+    if result:
+        print("done!")
